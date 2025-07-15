b7e5ae4ca4c6a0fe564c3d39b36cedbde8334705
gomahera-mymoota <iceman4u@naver.com>
2025-07-15 19:21:54 +0900

    end of chapter 3

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('Hello World~!'));
+app.get('/', (req, res) => res.send('Hello Web Server!!!'));
 
-app.listen(4000, () => console.log('Listening on port 4000!'));
+app.listen(port, () =>
+    console.log(`Server running at http://localhost:${port}`)
+)
