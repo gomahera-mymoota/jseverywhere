4d83e1cfb7a91a9fd23971630973976bc34485e2
gomahera-mymoota <iceman4u@naver.com>
2025-07-16 14:39:47 +0900

    ch04.03

--- api/src/index.js
+++ api/src/index.js
@@ -4,17 +4,48 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 
 dotenv.config();
 
+let notes = [
+    { id: '1', content: 'This is a note', author: 'IceMan4U' },
+    { id: '2', content: 'This is an another note', author: 'Adam Scott' },
+    { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' },
+];
+
 // 스키마 구성
 const typeDefs = `
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
   type Query {
     hello: String
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+
+  type Mutation {
+  newNote(content: String!): Note!
   }
 `;
 
 // 리졸버 구성
 const resolvers = {
     Query: {
-        hello: () => 'Hello World!~'
+        hello: () => 'Hello World!~',
+        notes: () => notes,
+        note: (parent, args) => notes.find(note => note.id === args.id),
+    },
+    Mutation: {
+        newNote: (parent, args) => {
+            let noteValue = {
+                id: String(notes.length + 1),
+                content: args.content,
+                author: 'IceMan4U'
+            };
+            notes.push(noteValue);
+            return noteValue;
+        }        
     }
 };
 
