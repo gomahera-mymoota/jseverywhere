5fc37aa3dbbe92c4c528f5357aaf58c0f4642207
gomahera-mymoota <iceman4u@naver.com>
2025-07-17 18:02:05 +0900

    ch06.03 노트 수정 및 삭제 추가

--- api/src/resolvers/mutation.js
+++ api/src/resolvers/mutation.js
@@ -2,5 +2,27 @@ export default {
     newNote: async (parent, args, { models }) => await models.Note.create({
         content: args.content,
         author: 'IceMan4U'
-    })
+    }),
+    deleteNote: async (parent, { id }, { models }) => {
+        try {
+            await models.Note.findOneAndDelete({ _id: id });
+            return true;
+        } catch (err) {
+            return false;
+        }
+    },
+    updateNote: async (parent, { content, id }, { models }) => 
+        await models.Note.findOneAndUpdate(
+            {
+                _id: id,
+            },
+            {
+                $set: {
+                    content
+                }
+            },
+            {
+                new: true
+            }
+        ),
 }
